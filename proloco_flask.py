1136f2499a61469b4b12ac88d7aa0aeaca8fbfb2
carlozanieri <carlo.zanieri@gmail.com>
2021-08-02 13:03:27 +0200

    Versione 1.0

--- proloco_flask.py
+++ proloco_flask.py
@@ -213,8 +213,8 @@ if __name__ == "__main__" or __name__ == "main":
             if os.path.isdir(os.getenv("TEMP_DIR")):
               print("Overriding tempdir for docker image")
               tempfile.tempdir = os.getenv("TEMP_DIR")
-    print("tempdir: " + "/home/carlo/proloco_flask/static/img/")
-    app.config['UPLOAD_FOLDER'] = "/home/carlo/proloco_flask/static/img/"
+    print("tempdir: " + "/srv/http/proloco_flask/static/img/")
+    app.config['UPLOAD_FOLDER'] = "/srv/http/proloco_flask/static/img/"
 
     # Below error if MAX_CONTENT_LENGTH is exceeded by upload
     # [error] 11#11: *1 readv() failed (104: Connection reset by peer) while reading upstream
